993c2df3672c98a1acc0f9bde2034d4bfab26805
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-04-20 19:30:46 -0300

    agr tem videoooo

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -1,94 +1,121 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom'
-import './filme.css'
-import api from '../../services/api';
-import { toast } from 'react-toastify'
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./filme.css";
+import api from "../../services/api";
+import { toast } from "react-toastify";
+import Iframe from "iframe";
 
 function Series() {
-    const { idfilme } = useParams();
-    const [filme, setFilme] = useState([]);
-    const [diretores, setDiretores] = useState([]);
-    const [loading, setLoading] = useState(true);
+  const { idfilme } = useParams();
+  const [filme, setFilme] = useState([]);
+  const [diretores, setDiretores] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [url, setUrl] = useState("");
 
-    useEffect(() => {
-        async function loadFilme() {
-            await api.get(`/movie/${idfilme}`, {
-
-            })
-                .then((response) => {
-                    setFilme([response.data]);
-                    //setDiretores(response.data.created_by)
-                    setLoading(false);
-                    //console.log(response.data)
-                })
-                .catch(() => {
-                    console.log("FILME NAO ENCONTRADO")
-                })
-        }
-
-        loadFilme();
-
-        //console.log(diretores.map(item => item.name))
-
-        return () => {
-            console.log("COMPONENTE FOI DESMONTADO")
-        }
-    }, [filme])
-
-    if (loading) {
-        return (
-            <div className="filme-info">
-                <h1>Carregando detalhes...</h1>
-            </div>
-        )
+  useEffect(() => {
+    async function loadFilme() {
+      await api
+        .get(`/movie/${idfilme}`, {})
+        .then((response) => {
+          setFilme([response.data]);
+          //setDiretores(response.data.created_by)
+          setLoading(false);
+          //console.log(response.data);
+        })
+        .catch(() => {
+          console.log("FILME NAO ENCONTRADo");
+        });
     }
 
-    function salvarFilme() {
-        const minhaLista = localStorage.getItem("@primeflix");
+    loadFilme();
 
-        let filmesSalvos = JSON.parse(minhaLista) || [];
+    //console.log(diretores.map(item => item.name))
 
-        const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id)
+    setUrl(`https://embedder.net/e/movie?imdb=${filme.map((item) => item.imdb_id)[0]}`);
+    // console.log(url);
+  }, [filme]);
 
-        if (hasFilme) {
-            toast.error('Esse filme ja está na lista')
-            return;
-        }
-
-        filmesSalvos.push(filme);
-        localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
-        toast.success('Filme salvo com sucesso!')
-
-    }
-
-
+  if (loading) {
     return (
-        <div className="filme-info">
-            {filme.map(item => {
-                return (
-                    <>
-                        <img className='background' src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`} alt={item.title} />
+      <div className="filme-info">
+        <h1>Carregando detalhes...</h1>
+      </div>
+    );
+  }
 
-                        <div className='box-info-filme'>
-                            <img className='poster' src={`https://image.tmdb.org/t/p/original/${item.poster_path}`} alt={item.title} />
+  function salvarFilme() {
+    const minhaLista = localStorage.getItem("@primeflix");
 
-                            <div className='info-text'>
-                                <h3>Sinopse</h3>
-                                <span>{item.overview}</span>
+    let filmesSalvos = JSON.parse(minhaLista) || [];
 
-                                <strong>Avalição: {item.vote_average} / 10</strong><br></br>
+    const hasFilme = filmesSalvos.some((filmesSalvo) => filmesSalvo.id === filme.id);
 
-                                <div className='btn-area'>
-                                    <button className='Salvar' onClick={salvarFilme}>Salvar</button>
-                                    <a target="blank" rel='external' href={`https://youtube.com/results?search_query=${item.title} filme trailer`} className='trailer'>Trailer</a>
-                                </div>
-                            </div>
-                        </div></>
-                )
-            })}
+    if (hasFilme) {
+      toast.error("Esse filme ja está na lista");
+      return;
+    }
 
-        </div>
-    )
+    filmesSalvos.push(filme);
+    localStorage.setItem("@primeflix", JSON.stringify(filmesSalvos));
+    toast.success("Filme salvo com sucesso!");
+  }
+
+  return (
+    <div className="filme-info">
+      {filme.map((item) => {
+        return (
+          <>
+            <img
+              className="background"
+              src={`https://image.tmdb.org/t/p/original/${item.backdrop_path}`}
+              alt={item.title}
+            />
+
+            <div className="box-info-filme">
+              <img
+                className="poster"
+                src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                alt={item.title}
+              />
+
+              <div className="info-text">
+                <h3>Sinopse</h3>
+                <span>{item.overview}</span>
+                <span>{item.imdb_id}</span>
+
+                <strong>Avalição: {item.vote_average} / 10</strong>
+                <br></br>
+
+                <div className="btn-area">
+                  <button className="Salvar" onClick={salvarFilme}>
+                    Salvar
+                  </button>
+                  <a
+                    target="blank"
+                    rel="external"
+                    href={`https://youtube.com/results?search_query=${item.title} filme trailer`}
+                    className="trailer"
+                  >
+                    Trailer
+                  </a>
+                </div>
+              </div>
+                {
+                  <iframe
+                    id="EmbedderContainer"
+                    src={url}
+                    width="100%"
+                    height="400px"
+                    allowfullscreen="allowfullscreen"
+                    frameborder="0"
+                  ></iframe>
+                }
+            </div>
+          </>
+        );
+      })}
+    </div>
+  );
 }
 
 export default Series;
