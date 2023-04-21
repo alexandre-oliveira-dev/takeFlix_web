fd76f313057dd3a27c4f04d1c9ac5ae359473a02
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-04-20 23:15:33 -0300

    agr tem videoooo

--- src/Pages/Filme/index.js
+++ src/Pages/Filme/index.js
@@ -3,7 +3,6 @@ import { useParams } from "react-router-dom";
 import "./filme.css";
 import api from "../../services/api";
 import { toast } from "react-toastify";
-import Iframe from "iframe";
 
 function Series() {
   const { idfilme } = useParams();
@@ -20,20 +19,21 @@ function Series() {
           setFilme([response.data]);
           //setDiretores(response.data.created_by)
           setLoading(false);
-          //console.log(response.data);
+          console.log(response.data);
         })
         .catch(() => {
           console.log("FILME NAO ENCONTRADo");
         });
+
     }
 
     loadFilme();
 
     //console.log(diretores.map(item => item.name))
 
-    setUrl(`https://embedder.net/e/movie?imdb=${filme.map((item) => item.imdb_id)[0]}`);
     // console.log(url);
-  }, [filme]);
+    setUrl(`https://embedder.net/e/movie?tmdb=${idfilme}`);
+  }, []);
 
   if (loading) {
     return (
@@ -77,39 +77,38 @@ function Series() {
                 src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
                 alt={item.title}
               />
-
-              <div className="info-text">
-                <h3>Sinopse</h3>
-                <span>{item.overview}</span>
-                <span>{item.imdb_id}</span>
-
-                <strong>Avalição: {item.vote_average} / 10</strong>
-                <br></br>
-
-                <div className="btn-area">
-                  <button className="Salvar" onClick={salvarFilme}>
-                    Salvar
-                  </button>
-                  <a
-                    target="blank"
-                    rel="external"
-                    href={`https://youtube.com/results?search_query=${item.title} filme trailer`}
-                    className="trailer"
-                  >
-                    Trailer
-                  </a>
-                </div>
+              <div className="boxVideo">
+                <iframe
+                  id="EmbedderContainer"
+                  src={url}
+                  width="100%"
+                  height={window.screen.width > 500 ? "400px" : "300px"}
+                  allowfullscreen="allowfullscreen"
+                  frameborder="0"
+                ></iframe>
               </div>
-                {
-                  <iframe
-                    id="EmbedderContainer"
-                    src={url}
-                    width="100%"
-                    height="400px"
-                    allowfullscreen="allowfullscreen"
-                    frameborder="0"
-                  ></iframe>
-                }
+            </div>
+            <div className="info-text">
+              <h3>Sinopse</h3>
+              <span>{item.overview}</span>
+              <span>{item.imdb_id}</span>
+
+              <strong>Avalição: {item.vote_average} / 10</strong>
+              <br></br>
+
+              <div className="btn-area">
+                <button className="Salvar" onClick={salvarFilme}>
+                  Salvar
+                </button>
+                <a
+                  target="blank"
+                  rel="external"
+                  href={`https://youtube.com/results?search_query=${item.title} filme trailer`}
+                  className="trailer"
+                >
+                  Trailer
+                </a>
+              </div>
             </div>
           </>
         );
--- src/Pages/Filmes/index.js
+++ src/Pages/Filmes/index.js
@@ -1,48 +1,109 @@
 import react, { useEffect, useState } from "react";
-import './style.css'
+import "./style.css";
 import api from "../../services/api";
 import Header from "../../components/Header";
 import { useParams } from "react-router-dom";
 
-
 export default function Filmes() {
+  const [data, setData] = useState([]);
+  const [page, setPage] = useState(1);
+  const [generes, setGeneres] = useState([]);
+  const [nomefilme, setNomefilme] = useState("");
 
-    const [data, setData] = useState([])
-    const [page, setPage] = useState(1)
+  const { number } = useParams("page");
 
-    const { number } = useParams('page')
+  useEffect(() => {
+    async function loadFilmes() {
+      await api.get(`/movie/popular?page=${number}`).then((value) => {
+        setData(value.data.results);
+      });
+    }
+    console.log(data);
+    loadFilmes();
+  }, []);
 
-    useEffect(() => {
-        async function loadFilmes() {
-            await api.get(`/movie/popular?page=${number}`)
-                .then((value) => {
-                    setData(value.data.results)
-                })
-        }
-        loadFilmes()
-    }, [page])
+  useEffect(() => {
+    api
+      .get("/genre/movie/list")
+      .then((value) => {
+        //console.log(value);
+        setGeneres(value.data.genres);
+        console.log(generes);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    return (
-        <div className="containerPageFilmes">
-            <Header></Header>
-            <section className="containerListFilms">
-                <div className='boxListfilmesFilmes'>
-                    {
-                        data.map(item => {
-                            return (
-                                <div key={item.id}>
-                                    <div className='item-filme'>
-                                        <img onClick={() => window.location.href = `/filme/${item.id}`} src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}></img>
-                                    </div>
-                                </div>
-                            )
-                        })
-                    }
+  return (
+    <div className="containerPageFilmes">
+      <Header></Header>
+      <div className="boxCategorias">
+        <input
+          type="search"
+          onChange={(e) => {
+            setNomefilme(e.target.value);
+          }}
+        ></input>
+        <button
+          onClick={async () => {
+            if (!nomefilme) {
+              return;
+            }
+            await api
+              .get(
+                `/search/movie?api_key=6488e6c48fd609153ab42d7243bf5670&query=${nomefilme}&page${number}&per_page=${50}`
+              )
+              .then((value) => {
+                setData(value.data.results);
+              });
+          }}
+        >
+          Pesquisar
+        </button>
+        <select
+          onChange={async (value) => {
+            window.location.pathname = `/filmes/page/${number}/${value.target.value}`;
+            await api
+              .get(
+                `/discover/movie?api_key=6488e6c48fd609153ab42d7243bf5670&with_genres=${
+                  value.target.value
+                }&page${number}&per_page=${30}`
+              )
+              .then((value) => {
+                setData(value.data.results);
+              });
+          }}
+        >
+          {generes.map((item) => {
+            return <option value={item.id}>{item.name}</option>;
+          })}
+        </select>
+      </div>
+      <section className="containerListFilms">
+        <div className="boxListfilmesFilmes">
+          {data.map((item) => {
+            return (
+              <div key={item.id}>
+                <div className="item-filme">
+                  <img
+                    onClick={() => (window.location.href = `/filme/${item.id}`)}
+                    src={`https://image.tmdb.org/t/p/original/${item.poster_path}`}
+                  ></img>
                 </div>
-            </section>
-            <div className="boxBtnNextPage">
-                <button id="btnNextPage" onClick={(() => window.location.href = `/filmes/page/${Number(number) + 1}`)}>Próxima página {Number(number) + 1}</button>
-            </div>
+              </div>
+            );
+          })}
         </div>
-    )
+      </section>
+      <div className="boxBtnNextPage">
+        <button
+          id="btnNextPage"
+          onClick={() => (window.location.href = `/filmes/page/${Number(number) + 1}`)}
+        >
+          Próxima página {Number(number) + 1}
+        </button>
+      </div>
+    </div>
+  );
 }
