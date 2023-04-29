38b91012198a445e996e44915d33306da0568527
Alexandretecsomobi <alexandredev.frontend@gmail.com>
2023-04-29 10:48:29 -0300

    features and fix

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,15 @@
 import { useEffect, useState } from "react";
 import "./header.css";
-import { FiUser } from "react-icons/fi";
+import { FiMenu, FiUser } from "react-icons/fi";
 
 function Header(props) {
   const [user, setUser] = useState();
   useEffect(() => {
-    const localstorage = JSON.parse(localStorage.getItem("@tokenTakeflix")) || '[]';
+    const localstorage = JSON.parse(localStorage.getItem("@tokenTakeflix")) || "[]";
     setUser(localstorage);
 
-    console.log(localstorage)
+    console.log(localstorage);
   }, []);
-
 
   return (
     <header style={{ background: props.color }} className="header">
@@ -35,10 +34,60 @@ function Header(props) {
         </div>
       </div>
 
-      {user ? (
+      <button
+        id="btnmobilemenu"
+        onClick={() => {
+          document.querySelector(".navBarMobile").classList.toggle("navBarMobileshow");
+        }}
+      >
+        <FiMenu color="#fff" size={25}></FiMenu>
+      </button>
+
+      <div className="navBarMobile">
+        {user !== "[]" ? (
+          <>
+            <label>
+              <FiUser color="#fff"></FiUser>olá {user?.email}
+            </label>
+            <button
+              id="sair"
+              onClick={() => {
+                localStorage.removeItem("@tokenTakeflix");
+                window.location.href = "/";
+              }}
+            >
+              sair
+            </button>
+          </>
+        ) : (
+          <>
+            <div>
+              <button onClick={() => (window.location.href = "/login")}>Login</button>
+            </div>
+            <div>
+              <button onClick={() => (window.location.href = "/cadastro")}>Cadastre-se</button>
+            </div>
+          </>
+        )}
+        <br></br>
+        <div>
+          <button onClick={() => (window.location.href = "/page/1")}>Home</button>
+        </div>
+        <div>
+          <button onClick={() => (window.location.href = "/filmes/page/1")}>Filmes</button>
+        </div>
+        <div>
+          <button onClick={() => (window.location.href = "/series/page/1")}>Séries</button>
+        </div>
+        <div>
+          <button onClick={() => (window.location.href = "/favoritos")}>Favoritos</button>
+        </div>
+      </div>
+
+      {user !== "[]" ? (
         <div className="userinfo">
           <label>
-            <FiUser color="#fff"></FiUser>olá {user?.name}
+            <FiUser color="#fff"></FiUser>olá {user?.email}
           </label>
           <button
             id="sair"
